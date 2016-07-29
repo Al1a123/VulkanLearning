b787bff87dbfa8688608eb709fad4252b9ce4707
Al1a123 <lovercallofduty@gmail.com>
2016-07-29 23:08:29 +0300

    Finished section "instance" chapter "Base Code"

--- VulkanLearning/VulkanLearning/main.cpp
+++ VulkanLearning/VulkanLearning/main.cpp
@@ -3,50 +3,12 @@
 #include <iostream>
 #include <stdexcept>
 #include <glfw3.h>
+#include <vector>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
 using namespace std;
-
-class TringleApplication
-{
-public:
-
-	const int WIDTH = 800;
-	const int HEIGHT = 600;
-
-	void run()
-	{
-		initWindow();
-		initVulkan();
-		mainLoop();
-	}
-
-private:
-
-	GLFWwindow* window;
-
-	void initWindow()
-	{
-		glfwInit();
-
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Learning", nullptr, nullptr);
-
-	}
-
-	void initVulkan()
-	{
-
-	}
-	void mainLoop()
-	{
-		while (!glfwWindowShouldClose(window))
-		{
-			glfwPollEvents();
-		}
-	}
-};
 
 template <typename T>
 class VDeleter {
@@ -93,6 +55,84 @@ private:
 	}
 };
 
+class TringleApplication
+{
+public:
+
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+	}
+
+private:
+    VDeleter<VkInstance> instance{ vkDestroyInstance };
+
+	GLFWwindow* window;
+
+	void initWindow()
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Learning", nullptr, nullptr);
+
+	}
+
+	void initVulkan()
+	{
+		createInstance();
+	}
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	void createInstance()
+	{
+		VkApplicationInfo appinfo = {};
+		appinfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appinfo.pApplicationName = "Hello Tringle";
+		appinfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appinfo.pEngineName = "No Engine";
+		appinfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appinfo.apiVersion = VK_API_VERSION;
+
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appinfo;
+ 
+		unsigned int glfwextensioncount = 0;
+		const char** glfwextensionnames = glfwGetRequiredInstanceExtensions(&glfwextensioncount);
+		createInfo.enabledExtensionCount = glfwextensioncount;
+		createInfo.ppEnabledExtensionNames = glfwextensionnames;
+		createInfo.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+			throw runtime_error("vulkan couldn't create instance");
+
+        uint32_t extensionCount;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		vector<VkExtensionProperties> _vk_extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, _vk_extensions.data());
+		cout << "Required vulkan extensions :" << endl;
+		for (const auto& extension : _vk_extensions  )
+			cout << "\t" << extension.extensionName << endl;
+
+
+
+	}
+
+};
+
+
 int main()
 {
 	TringleApplication app;
@@ -105,6 +145,7 @@ int main()
 		cerr << e.what() << endl;
 		return EXIT_FAILURE;
 	}
+
 
 	return EXIT_SUCCESS;
 
